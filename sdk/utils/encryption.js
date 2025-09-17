3163b7bb54278631d951735ae41bbf42e9f1630b
Adnan UL Mustafa <150100234+Adnanmd76@users.noreply.github.com>
2025-09-17 12:21:19 +0500

    feat(utils): add AES encryption and decryption helpers

---
+++ sdk/utils/encryption.js
@@ -0,0 +1,16 @@
+// encryption.js
+import crypto from 'crypto';
+
+export function encryptData(data, secret) {
+  const cipher = crypto.createCipher('aes-256-cbc', secret);
+  let encrypted = cipher.update(data, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return encrypted;
+}
+
+export function decryptData(encrypted, secret) {
+  const decipher = crypto.createDecipher('aes-256-cbc', secret);
+  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  return decrypted;
+}
